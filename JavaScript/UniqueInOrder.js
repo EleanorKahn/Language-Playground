9f75c922ad056750f018c3ea7a0ecfd1d99a4ef9
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-04-20 21:36:14 -0700

    uniqueInOrder

---
+++ JavaScript/UniqueInOrder.js
@@ -0,0 +1,4 @@
+var uniqueInOrder=function(iterable){
+    //spread will spread string into iterable array of chars, if iterable is type String
+    return [...iterable].filter((unique, idx) => unique !== iterable[idx+1]);
+  }
