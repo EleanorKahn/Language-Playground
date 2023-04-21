d6a20c1c08ceb856eec23277596061dbf4b42bd1
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-04-21 13:06:30 -0700

    optional properties and assigned variables

---
+++ TypeScript/optionalProp.ts
@@ -0,0 +1,21 @@
+//Challenge from Totally Typescript
+//Answer by Eleanor
+
+export const getName = (first: string, last?: string) => {
+  if (last) {
+    return `${first} ${last}`;
+  }
+  return first;
+};
+
+// it("Should work with just the first name", () => {
+//   const name = getName("Matt");
+
+//   expect(name).toEqual("Matt");
+// });
+
+// it("Should work with the first and last name", () => {
+//   const name = getName("Matt", "Pocock");
+
+//   expect(name).toEqual("Matt Pocock");
+// });
