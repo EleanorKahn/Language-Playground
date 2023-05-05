a20066d4d53fe3c0ab68c6c77364019da1c1864d
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-05-04 22:17:28 -0700

    dynamic keys ts

--- TypeScript/indexSignitures.ts
+++ TypeScript/indexSignitures.ts
@@ -26,12 +26,3 @@ const remove = (id: string) => {
 
 //   expect(cache.cache["123"]).toEqual("Matt");
 // });
-
-// it("Should remove values from the cache", () => {
-//   const cache = createCache();
-
-  cache.add("123", "Matt");
-  cache.remove("123");
-
-//   expect(cache.cache["123"]).toEqual(undefined);
-// });
