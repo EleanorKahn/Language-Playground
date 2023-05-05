e730f8c2f0cc24daa6754658881ea63b99494d97
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-05-05 15:58:20 -0700

    coercing errors in a try catch block

---
+++ TypeScript/errorsInTryCtch.ts
@@ -0,0 +1,20 @@
+//Challenge from Totally Typescript
+//Answer by Eleanor
+
+//import { expect, it } from "vitest";
+
+const tryCatchDemo = (state: "fail" | "succeed") => {
+  try {
+    if (state === "fail") {
+      throw new Error("Failure!");
+    }
+  } catch (e) {
+      if (e instanceof Error) {
+      return e.message;
+      }
+  }
+};
+
+// it("Should return the message when it fails", () => {
+//   expect(tryCatchDemo("fail")).toEqual("Failure!");
+// });
